f65a68e0e66455296a3579d5594d827338af4e25
Peter Eberle <46387202+petereberle@users.noreply.github.com>
2021-05-10 09:09:21 -0700

    added menu system

--- content/_index.md
+++ content/_index.md
@@ -1,5 +1,4 @@
 ---
 reelVideo:
   - video: /images/uploads/Barney_Slide_imovie.mp4
-  - video: /images/uploads/Barney_Slide_imovie.mp4
 ---
--- static/css/scripts.js
+++ static/css/scripts.js
@@ -2,4 +2,40 @@ $(document).ready(function(){
 	
 	$('#wwf_logo>svg').addClass('fill_change')
 
+		$("#hamburger").on("click", function(){
+
+		$('#side_menu_wrapper').animate({marginRight: '100vw'}, 500);
+
+		// $('header').css('borderBottom' , 'solid .012px white');
+
+		$('#side_menu_wrapper').addClass("active");
+
+		$('#content').css('filter', 'blur(1px)');
+
+		$('#contact_content').css('filter', 'blur(1px)');
+
+		// $('#menu').css('display', 'none');
+
+	});
+
+	$("#xit").on("click", function(){
+
+		if ( $('#side_menu_wrapper').hasClass('active') ){
+
+			$('#side_menu_wrapper').animate({marginRight: '0'}, 500);
+
+			// $('header').css('borderBottom' , 'solid .012px #bebec1');
+
+			$('#content').css('filter', 'blur(0px)');
+
+			$('#contact_content').css('filter', 'blur(0px)');
+
+			$('#side_menu_wrapper').removeClass("active");
+
+			// $('#menu').css('display', 'flex');
+
+		}
+
+	});
+
 })
