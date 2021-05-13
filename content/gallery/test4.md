95e7063c26222dc489475129d352f850bf465bf0
Peter Eberle <46387202+petereberle@users.noreply.github.com>
2021-05-13 14:03:53 -0700

    infinite pagination

--- content/gallery/test4.md
+++ content/gallery/test4.md
@@ -4,7 +4,7 @@ artist: Test
 year: test
 material: test
 attribution: test
-1_image: /images/uploads/5127.1_mithra_process_42.jpg
+featured: /images/uploads/5127.1_mithra_process_42.jpg
 thumbnailImages:
   - image: /images/uploads/5127.1_final_photos_01.jpg
 ---
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	//Menu Behavior
 	
 		$("#hamburger").on("click", function(){
 
@@ -36,4 +38,42 @@ $(document).ready(function(){
 
 	});
 
-})
+	//Infinite Pagination
+
+	 //we will bind an on-click handler for the next page button, but
+    //handled by the body. This will allow us to replace the button
+	$('body').off('click').on('click', '#arrow_right', function(e) {
+		e.preventDefault(); //block the link click from changing the page
+		//fire off an AJAX request to load the next page
+		var nextPageUrl = $(this).prop('href');
+		$.ajax({
+			url: nextPageUrl,
+			dataType: 'text', //this is to avoid jQuery running any <script> tags
+			success: function(html) {
+				var currentContainer = $('.project_slide');
+				//create a temporary hidden element to attach the created document
+				//this is the simplest jQuery way to do this safely.
+				var tempDiv = $('<div>').appendTo(document.body).css('display', 'none');
+				tempDiv[0].innerHTML = html;
+				//find the container element in the new document
+				var newContainer = tempDiv.find('.project_slide');
+				//replace the container on the current page with the new one
+				currentContainer.replaceWith(newContainer);
+				//remove the temporary element
+				tempDiv.remove();
+				//update the URL bar
+				if (window.history.pushState)
+				{
+					window.history.pushState(null, null, nextPageUrl);
+				}
+				console.log("Successfully changed page to " + nextPageUrl);
+			},
+			error: function(xhr, status, error) {
+				console.error(xhr, status, error);
+				//default to non-JS behaviour and click-through as normal
+				window.location.href = nextPageUrl;
+			}
+		})
+	});
+
+});
